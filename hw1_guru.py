4fa5ad5f42728fba168a21302606455db9d92a03
Rodion Philippov <rodik.rahmanov@gmail.com>
2024-06-13 17:28:45 +0500

    !!!!!

--- hw1_guru.py
+++ hw1_guru.py
@@ -2,7 +2,7 @@ from selene import browser, be, have
 import time
 
 def test_google_positive():
-        #Тест поиска гугл
+        #Тест поиска гугл!!!!!
         browser.open('https://google.com')
         browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
         browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
